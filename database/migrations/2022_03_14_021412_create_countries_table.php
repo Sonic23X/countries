acbe7a266788ebd539c8d6e4ce9fa2ab71509c0f
Alfred Ortega <omar.alfredo49@gmail.com>
2022-03-14 03:38:47 -0600

    Fix migration

--- database/migrations/2022_03_14_021412_create_countries_table.php
+++ database/migrations/2022_03_14_021412_create_countries_table.php
@@ -16,8 +16,8 @@ return new class extends Migration
         Schema::create('countries', function (Blueprint $table) {
             $table->string('code')->primary();
             $table->string('name');
-            $table->string('continent');
-            $table->bigInteger('population');
+            $table->string('continent')->nullable();
+            $table->bigInteger('population')->nullable();
             $table->timestamps();
         });
     }
